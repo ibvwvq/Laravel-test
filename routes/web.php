12e4d074de12b6ffb4373d8ce185aacf3ab0c6d5
ibvwvq <ikramova_arina@mail.ru>
2023-07-26 12:08:40 +0300

    Создание еще одного контроллера и action home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'home']);
 
-
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/home', [HomeController::class, 'home']);
 
 Route::get('/about', function () {
     return view('about');
