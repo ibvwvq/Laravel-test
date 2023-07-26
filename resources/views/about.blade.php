988a4704e410104e0c65d6808c391cdb4777fd3b
ibvwvq <ikramova_arina@mail.ru>
2023-07-26 09:56:44 +0300

    страницы главная и о нас, добавления title

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title')
+    О нас
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>О нас</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias assumenda corporis, delectus error facilis harum, id illo inventore itaque magni molestias natus numquam optio quisquam, quod repellendus totam voluptatem.</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
 
 Route::get('/user/{id}', function ($id) {
