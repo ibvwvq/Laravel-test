053f9bb72cad172e8e2e57fd94b8b934f90f8d67
ibvwvq <ikramova_arina@mail.ru>
2023-07-26 05:29:46 +0300

    создание вида для вывода на страницах верхней панели навигации

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body class="bg-dark">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-dark ">
+        <div class="col-md-3 mb-2 mb-md-0" >
+            <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none text-white">
+              <h5 class="ms-2">Test Project</h5>
+            </a>
+        </div>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="/" class="nav-link px-2 link-secondary text-white">Главная</a></li>
+            <li><a href="/about" class="nav-link px-2 text-white">О нас</a></li>
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <button type="button" class="btn btn-danger me-2" href="/review">Отзывы</button>
+        </div>
+    </header>
+</body>
+</html>
